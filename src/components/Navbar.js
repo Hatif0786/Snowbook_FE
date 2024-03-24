8961c81438ec0195bc5c9fb02a6e1b9b9e0aff7c
Hatif0786 <hatifkhuld90@gmail.com>
2024-03-24 22:48:53 +0530

    ver10.0.0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,93 +102,95 @@ const Navbar = () => {
               )}
             </ul>
             
-            {!islogged && !logged && location.pathname !== "/signup" && (
-            <button
-              onClick={handleRegisterClick}
-              style={{ marginRight: "7px", backgroundColor: "#d17842" }}
-            >
-              <span>Register</span>
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 72 75"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="37"
-                  cy="37"
-                  r="35.5"
-                  stroke="black"
-                  strokeWidth="3"
-                ></circle>
-                <path
-                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                  fill="black"
-                ></path>
-              </svg>
-            </button>
-          )}
-
-          <br/>
-
-          {!islogged && !logged && location.pathname !== "/login" && (
-            <button
-              onClick={handleLoginClick}
-              style={{ backgroundColor: "#3ba8d4" }}
-            >
-              <span>Login</span>
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 72 75"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="37"
-                  cy="37"
-                  r="35.5"
-                  stroke="black"
-                  strokeWidth="3"
-                ></circle>
-                <path
-                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                  fill="black"
-                ></path>
-              </svg>
-            </button>
-          )}
+            <div className="flex-end">
+              
+                  {!islogged && !logged && location.pathname !== "/signup" && (
+                  <button
+                    onClick={handleRegisterClick}
+                    style={{ marginRight: "7px", backgroundColor: "#d17842", marginTop:"15px" }}
+                  >
+                    <span>Register</span>
+                    <svg
+                      width="30"
+                      height="30"
+                      viewBox="0 0 72 75"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="37"
+                        cy="37"
+                        r="35.5"
+                        stroke="black"
+                        strokeWidth="3"
+                      ></circle>
+                      <path
+                        d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                        fill="black"
+                      ></path>
+                    </svg>
+                  </button>
+                )}
 
 
-          {islogged && logged && (
-            <button
-              onClick={handleLogoutClick}
-              className="btn btn-warning"
-              style={{borderRadius: "25px"}}
-            >
-              <span>Logout</span>
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 72 75"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="37"
-                  cy="37"
-                  r="35.5"
-                  stroke="black"
-                  strokeWidth="3"
-                ></circle>
-                <path
-                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                  fill="black"
-                ></path>
-              </svg>
-            </button>
-          )}
+                {!islogged && !logged && location.pathname !== "/login" && (
+                  <button
+                    onClick={handleLoginClick}
+                    style={{ backgroundColor: "#3ba8d4", marginTop:"15px" }}
+                  >
+                    <span>Login</span>
+                    <svg
+                      width="30"
+                      height="30"
+                      viewBox="0 0 72 75"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="37"
+                        cy="37"
+                        r="35.5"
+                        stroke="black"
+                        strokeWidth="3"
+                      ></circle>
+                      <path
+                        d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                        fill="black"
+                      ></path>
+                    </svg>
+                  </button>
+                )}
+
+
+                {islogged && logged && (
+                  <button
+                    onClick={handleLogoutClick}
+                    className="btn btn-warning"
+                    style={{marginRight: "7px",borderRadius: "25px", marginTop:"10px"}}
+                  >
+                    <span>Logout</span>
+                    <svg
+                      width="30"
+                      height="30"
+                      viewBox="0 0 72 75"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="37"
+                        cy="37"
+                        r="35.5"
+                        stroke="black"
+                        strokeWidth="3"
+                      ></circle>
+                      <path
+                        d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                        fill="black"
+                      ></path>
+                    </svg>
+                  </button>
+                )}  
+            </div>
           </div>    
         </div>
       </nav>
